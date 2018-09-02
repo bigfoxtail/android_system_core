c5045c3543b02c019a135a526632ac8e647c434a
Martincz Gao <martincz@mokeedev.com>
2018-09-02 16:29:38 +0800

    core: mokee-ify
    Change-Id: If8b535e0e800b3397d5aa1c64cddc273113545ef

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -173,7 +173,7 @@ static const struct fs_path_config android_files[] = {
     // the following file is INTENTIONALLY set-uid, but it
     // is NOT included on user builds.
     { 06755, AID_ROOT,      AID_ROOT,      0, "system/xbin/procmem" },
-    // lineage's daemonized su doesn't need the setuid bit
+    // mokee's daemonized su doesn't need the setuid bit
     { 00755, AID_ROOT,      AID_SHELL,     0, "system/xbin/su" },
 
     // the following files have enhanced capabilities and ARE included
